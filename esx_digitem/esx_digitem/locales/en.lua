b0a53f8d6e69e0b11bd54dab40449efd106f004e
MINO <55931955+weimino123@users.noreply.github.com>
2020-05-16 05:32:37 +0800

    Delete en.lua

--- esx_digitem/esx_digitem/locales/en.lua
+++
@@ -1,7 +0,0 @@
-Locales['en'] = {
-	["press_dig"] = "Press ~INPUT_CONTEXT~ to dig ~y~%s",
-	["got"] = "You got ~y~%sx ~b~%s",
-	["tool_broken"] = "Your ~y~%s ~s~is broken!",	
-	["no_tools"] = "You have no ~r~%s ~s~to dig",
-	["not_enough_place"] = "Not enough space to put ~y~%sx ~b~%s to your bag"
-}
