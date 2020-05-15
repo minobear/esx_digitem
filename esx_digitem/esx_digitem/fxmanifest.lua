b7b43ebd1b3824ef292bf14d97eba162c27db14c
MINO <55931955+weimino123@users.noreply.github.com>
2020-05-16 05:30:41 +0800

    Add files via upload

---
+++ esx_digitem/esx_digitem/fxmanifest.lua
@@ -0,0 +1,19 @@
+-- Created by @MINO https://forum.cfx.re/u/mino1
+fx_version "adamant"
+game "gta5"
+
+client_script {
+	"@es_extended/locale.lua",	
+	"client/main.lua",
+	"config.lua",
+	"locales/tw.lua",
+	"locales/en.lua"
+}
+
+server_script {
+	"@es_extended/locale.lua",
+	"server/main.lua",
+	"config.lua",
+	"locales/tw.lua",
+	"locales/en.lua"
+}
